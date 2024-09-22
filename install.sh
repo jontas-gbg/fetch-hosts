55c222bd6a050630d280e0865bd4d674fd2fdfd3
jontas-gbg <jontas@gmx.com>
2024-09-22 21:05:46 +0200

    Changed from system to user systemd

--- install.sh
+++ install.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# Konstanter
+# Const
 SCRIPT_DIR="$HOME/.local/bin"
-SYSTEMD_DIR="/etc/systemd/system"
-SCRIPT_NAME="get_hosts-file.sh"
+SYSTEMD_DIR="$HOME/.config/systemd/user"
+SCRIPT_NAME="gethosts"
 TIMER_NAME="update-hosts.timer"
 SERVICE_NAME="update-hosts.service"
 
-# Skapar "bin" om den inte redan finns
+# Create local bin if not exists
 mkdir -p "$SCRIPT_DIR"
 
-# Kopierar scriptet som systemd-servicen kör enligt systemd-timer-schema
-cp "$SCRIPT_NAME" "$SCRIPT_DIR" && chmod +x "$SCRIPT_DIR/$SCRIPT_NAME"
+# Copy script, timer and service to it's directories
+cp "$SCRIPT_NAME" "$SCRIPT_DIR" && chmod +x "$SCRIPT_DIR/$SCRIPT_NAME" &&
+cp update-hosts.{timer,service} "$SYSTEMD_DIR"
 
-# Kopierar systemd-filer, aktiverar timern och hämtar hosts-filen
-sudo cp update-hosts.{timer,service} "$SYSTEMD_DIR"
-sudo systemctl daemon-reload
-sudo systemctl enable --now "$TIMER_NAME"
-sudo systemctl start "$SERVICE_NAME"
+# reload, enable and get hosts file 
+systemctl --user daemon-reload &&
+systemctl --user enable --now "$TIMER_NAME" &&
+systemctl --user start "$SERVICE_NAME"
 
-# Loggar en lyckad installation
-logger -t install_script "Installationen av $SCRIPT_NAME samt systemd-filer är klara och hosts-filen är på plats"
